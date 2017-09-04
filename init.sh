a29aee59462bc5028a842183c17805812b5d7bdd
Hendrix Costa <hendrixcosta@gmail.com>
2017-09-04 09:04:10 -0300

    [ADD] Script para virtualenv

--- init.sh
+++ init.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
+
+virtualenv .
+
+source bin/activate
+
+pip install Sphinx==1.3
+
+pip install hieroglyph sphinx_rtd_theme
+
+sudo apt-get install texlive-lang-portuguese
+
 make dir=docs slides html
+
+
